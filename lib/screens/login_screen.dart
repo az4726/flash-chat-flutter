fbcc3efd8249044d5b0640e685e633fd4f94c05c
Azhar Zaman <azhar.zaman99@gmail.com>
2020-04-05 16:15:37 +0100

    Implement registration using Firebase

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -8,6 +8,9 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  String email;
+  String password;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,8 +32,10 @@ class _LoginScreenState extends State<LoginScreen> {
               height: 48.0,
             ),
             TextField(
+              keyboardType: TextInputType.emailAddress,
               onChanged: (value) {
                 //Do something with the user input.
+                email = value;
               },
               decoration:
                   kTextFieldDecoration.copyWith(hintText: 'Enter Email'),
@@ -39,8 +44,10 @@ class _LoginScreenState extends State<LoginScreen> {
               height: 8.0,
             ),
             TextField(
+              obscureText: true,
               onChanged: (value) {
                 //Do something with the user input.
+                password = value;
               },
               decoration:
                   kTextFieldDecoration.copyWith(hintText: 'Enter Password'),
@@ -50,7 +57,7 @@ class _LoginScreenState extends State<LoginScreen> {
             ),
             Hero(
               tag: 'Login',
-              child: RoundedButton(Colors.lightBlueAccent, null, 'Log In'),
+              child: RoundedButton(Colors.lightBlueAccent, () {}, 'Log In'),
             ),
           ],
         ),
--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flash_chat/components/rounded_button.dart';
 import 'package:flash_chat/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class RegistrationScreen extends StatefulWidget {
   @override
@@ -8,6 +9,11 @@ class RegistrationScreen extends StatefulWidget {
 }
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
+  final _auth = FirebaseAuth.instance;
+
+  String email;
+  String password;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,8 +35,10 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
               height: 48.0,
             ),
             TextField(
+              keyboardType: TextInputType.emailAddress,
               onChanged: (value) {
                 //Do something with the user input.
+                email = value;
               },
               decoration:
                   kTextFieldDecoration.copyWith(hintText: 'Enter Email'),
@@ -39,8 +47,10 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
               height: 8.0,
             ),
             TextField(
+              obscureText: true,
               onChanged: (value) {
                 //Do something with the user input.
+                password = value;
               },
               decoration:
                   kTextFieldDecoration.copyWith(hintText: 'Enter Password'),
@@ -50,7 +60,17 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
             ),
             Hero(
               tag: 'Register',
-              child: RoundedButton(Colors.blueAccent, null, 'Register'),
+              child: RoundedButton(Colors.blueAccent, () async {
+                try {
+                  final newUser = await _auth.createUserWithEmailAndPassword(
+                      email: email, password: password);
+                  if (newUser != null) {
+                    Navigator.pushNamed(context, 'chat_screen');
+                  }
+                } catch (e) {
+                  print(e);
+                }
+              }, 'Register'),
             ),
           ],
         ),
